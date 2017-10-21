18046af651736137fe58a56578638bbecd33247e
Juan Volpintesta <jvolpintesta@gmail.com>
2017-10-21 13:51:25 +0100

    create methods to extract all the information

--- src/main/java/DataExtractor.java
+++ src/main/java/DataExtractor.java
@@ -1,3 +1,5 @@
+import org.apache.commons.io.FilenameUtils;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -11,5 +13,15 @@ public class DataExtractor {
         BasicFileAttributes attr = Files.readAttributes(file, BasicFileAttributes.class);
         return "size: " + attr.size();
     }
-    
+    public String getMimeType(Path file) throws IOException {
+        return "MIME type: " + Files.probeContentType(file);
+    }
+
+    public String getName(Path file) throws IOException {
+        return "file name: " + file.getFileName();
+    }
+
+    public String getFileExtension(Path file) throws IOException {
+        return "file extension: " + FilenameUtils.getExtension(getName(file));
+    }
 }
